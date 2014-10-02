95d81ccade66015d40f84e2d6d51af309893a275
Carlgo11 <carlgo11@carlgo11.com>
2014-10-02 14:09:19 +0200

    Cleanup

--- contact.php
+++ contact.php
@@ -10,42 +10,42 @@ include './resources/head-mainpage.php';
                     <h2>Contact
                     </h2>
                     <p>Fill in a message to us below and we'll get back to you as soon as possible.</p>
-                    <form method="POST" action="" style="width: 200px;vertical-align: middle;margin-left: 120px">
-                        <?php
-                        session_start();
-                        
-                            if (!class_exists('KeyCAPTCHA_CLASS')) {
-                                include('./resources/keycaptcha.php');
-                            }
-                            $kc_o = new KeyCAPTCHA_CLASS();
-                            if (isset($_POST['capcode'])) {
-                                if ($kc_o->check_result($_POST['capcode'])) {
-                                    $sent = true;
-                                    $email = $_POST['email'];
-                                    $message = $_POST['message'];
-                                    mail("carlgo11@carlgo11.com", "New mail from ".$email, "Hello,\nYou've recived a new question from: ".$email."\n\nMessage:".$message);
-                                   echo '<div class="alert alert-success" role="alert">Email sent!</div>';
-                                } else {
-                                   // incorrect
-                                }
-                            }
-                        if(!isset($sent) || !$sent){
+                    <?php
+                    session_start();
+
+                    if (!class_exists('KeyCAPTCHA_CLASS')) {
+                        include('./resources/keycaptcha.php');
+                    }
+                    $kc_o = new KeyCAPTCHA_CLASS();
+                    if (isset($_POST['capcode'])) {
+                        if ($kc_o->check_result($_POST['capcode'])) {
+                            $sent = true;
+                            $email = $_POST['email'];
+                            $message = $_POST['message'];
+                            mail("carlgo11@carlgo11.com", "New mail from " . $email, "Hello,\nYou've recived a new question from: " . $email . "\n\nMessage:\n" . $message);
+                            echo '<div class="alert alert-success" role="alert" style="height: 100px">Email sent!<br><br>A reply will be sent to:' . $email . '.</div>';
+                        } else {
+                            // incorrect
+                        }
+                    }
+                    if (!isset($sent) || !$sent) {
                         if (!class_exists('KeyCAPTCHA_CLASS')) {
                             include('./resources/keycaptcha.php');
                         }
                         $kc_o = new KeyCAPTCHA_CLASS();
+                        echo '<form method="POST" action="" style="width: 200px;vertical-align: middle;margin-left: 120px">';
                         echo $kc_o->render_js();
-                        }
-                        ?>
-                        <input type='email' id='email' name="email" placeholder="Your email" required="" style="margin-bottom: 20px;margin-top: 20px" class="form-control"/>
+                        echo '<input type="email" id="email" name="email" placeholder="Your email" required="" style="margin-bottom: 20px;margin-top: 20px" class="form-control"/>
                         <textarea rows="4" id="message" name="message" cols="50" required="" style="margin-bottom: 20px" placeholder="Message"></textarea>
-                        
-
                         <input type="hidden" name="capcode" id="capcode" value="false" />
-                        <input type="submit" value="Send" id="postbut" class="btn btn-success" />
-                    </form>
+                        <input type="submit" value="Send" id="postbut" class="btn btn-success" />';
+                        echo '</form>';
+                    }
+                    ?>
+
+
                 </div>
-<?php include './resources/footer.php'; ?>
+                <?php include './resources/footer.php'; ?>
             </div>
         </div>
     </div>
