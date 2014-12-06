a077c7717772c18ff0e17ae6485de767a0254692
Carlgo11 <carlgo11@carlgo11.com>
2014-12-06 21:34:13 +0100

    change mysql-username

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $conf['upload-dir'] = "./uploads/";
 $conf['title'] = "UpLoadMe";
 $conf['mysql-url'] = "localhost";
-$conf['mysql-user'] = "UploadMe";
+$conf['mysql-user'] = "uploadme";
 $conf['mysql-password'] = "dEHyePMLDVmBxPz7";
 $conf['mysql-db'] = "uploadme";
 $conf['mysql-table'] = "upload";
