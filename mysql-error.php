6054e674bf5d085f07b4c064e34a6fd70519639d
Carlgo11 <carlgo11@carlgo11.com>
2014-12-07 18:10:08 +0100

    add more images :D

--- mysql-error.php
+++ mysql-error.php
@@ -5,10 +5,9 @@ include './resources/head-mainpage.php';
     <div class="site-wrapper">
         <div class="site-wrapper-inner">
             <div class="cover-container">
-
                 <div class="inner go">
                     <h2>Whoops, that wasn't supposed to happen!</h2><br>
-                    <?php echo '<img src="resources/media/errors/'.rand(1,7).'.jpg" width="500"/>';?><br><br>
+                    <?php echo '<img src="resources/media/errors/'.rand(1,11).'.jpg" width="500"/>';?><br><br>
                     <h4>But do not fear! Out tech-kittens have already been notified!</h4>
                     <p>Most likely our storage-server is down. Try again soon.</p>
                 </div>
