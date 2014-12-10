c267413b95894f69adc58ac8dd0be0e136de8b69
Carlgo11 <carlgo11@carlgo11.com>
2014-12-10 17:36:29 +0100

    cleanup of code

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
-
 include 'config.php';
-
-include './resources/upload.php';
+include './resources/head-mainpage.php';
+?>
+<body>
+    <div class="site-wrapper">
+        <div class="site-wrapper-inner">
+            <div class="cover-container">
+                <?php
+                include './resources/navbar.php';
+                getNavBar("home");
+                ?>
+                <div class="inner go">
+                    <h1>UpLoadMe, Uploading made simple</h1>
+                    <p class="lead">
+                        With UpLoadMe you can share your personal files<br>with friends without needing FTP to a server or giving out your personal information to a company like Google or Dropbox.<br>
+                        Simply Drop the file in the green area and click Upload
+                    </p>
+                    <form action="./upload_file.php" method="post" enctype="multipart/form-data">
+                        <center>
+                            <input class="btn btn-success" style="width: 500px; height: 100px"  data-filename-placement="inside" id="file" name="file" type="file" /><br>
+                            <br>
+                            <input type="submit" name="submit" value="Upload" id="submit" class="btn btn-lg btn-default" disabled>
+                        </center>
+                    </form>
+                </div>
+                <?php include './resources/footer.php'; ?>
+            </div>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/docs.min.js"></script>
+    <script src="./js/disable.js"></script>
+</body>
+</html>
