2dbe561f9a413882c039bd2c7236ee37a6da71d8
Carlgo11 <carlgo11@carlgo11.com>
2015-01-09 14:25:13 +0100

    added config.php example

---
+++ config.example.php
@@ -0,0 +1,22 @@
+<?php
+
+# Site title. Applies for all pages importing res/head.php
+$conf['title'] = "UpLoadMe";
+
+# Mysql url
+$conf['mysql-url'] = "example.com";
+
+# Mysql username
+$conf['mysql-user'] = "uploadme";
+
+# Mysql password
+$conf['mysql-password'] = "password";
+
+# Mysql database
+$conf['mysql-db'] = "uploadme";
+
+# Mysql uploads table. (The table containing all uploaded files)
+$conf['mysql-table'] = "uploads";
+
+# Contact email. (Used in contact.php)
+$conf['email-reciver'] = "admin@example.com";
