08ea3526d281798154544b98871d5f5fd0f628de
psgs <fpigerre@gmail.com>
2015-01-14 13:35:52 +1000

    Improve mysql-error styling

--- mysql-error.php
+++ mysql-error.php
@@ -2,18 +2,17 @@
 include './res/head.php';
 ?>
 <body>
-<div class="site-wrapper">
-    <div class="site-wrapper-inner">
-        <div class="cover-container">
-            <div class="inner go">
-                <h2>Whoops, that wasn't supposed to happen!</h2><br>
-                <?php echo '<img src="./res/media/errors/' . rand(1, 11) . '.jpg" width="500"/>'; ?><br><br>
-                <h4>But do not fear! Out tech-kittens have already been notified!</h4>
+<nav>
+    <a class="brand" href="./">
+        <h3>UpLoadMe<img height="32" width="32" src="/res/media/logo.png"></h3>
+    </a>
+</nav>
+<div class="mysql-content">
+    <h2>Whoops, that wasn't supposed to happen!</h2><br>
+    <?php echo '<img src="./res/media/errors/' . rand(1, 11) . '.jpg" width="500"/>'; ?><br><br>
+    <h4>But do not fear! Out tech-kittens have already been notified!</h4>
 
-                <p>Most likely our storage-server is down. Try again soon.</p>
-            </div>
-        </div>
-    </div>
+    <p>Most likely our storage-server is down. Try again soon.</p>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="./js/bootstrap.min.js"></script>
