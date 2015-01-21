e71e637db72697226cb26d2475d34dcbd5d5bae9
psgs <fpigerre@gmail.com>
2015-01-21 10:28:33 +1000

    Fix general CSS issues
    Update request-password.php layout, ensure logo is display correctly on
    mysql-error.php, increase glyphicon size.

--- res/request-password.php
+++ res/request-password.php
@@ -2,41 +2,36 @@
 include './res/head.php';
 ?>
 <body>
-<div class="site-wrapper">
-    <div class="site-wrapper-inner">
-        <div class="cover-container">
-            <?php
-            include './res/navbar.php';
-            getNavBar();
-            ?>
-            <div class="inner go">
-                <?php if (isset($error) && $error == 0) {
-                    ?>
-                    <div class="alert alert-danger"><h3>Incorrect password!</h3>
+<?php
+include './res/navbar.php';
+getNavBar();
+?>
+<div class="content">
+    <?php if (isset($error) && $error == 0) {
+        ?>
+        <div class="alert alert-danger"><h3>Incorrect password!</h3>
 
-                        <p>The password you entered is incorrect. Please try again.</p></div>
-                <?php
-                }
-                ?>
-                <h1>Please enter the decryption password.</h1><br>
+            <p>The password you entered is incorrect. Please try again.</p></div>
+    <?php
+    }
+    ?>
+    <h1 class="home">Please enter the decryption password.</h1><br>
 
-                <p class="lead">The file you requested is encrypted with a password by the creator.</p>
+    <p class="lead">The file you requested is encrypted with a password by the creator.</p>
 
-                <p>Please enter the decryption password in the box below:</p>
-                <center>
-                    <form action="" method="POST" enctype="multipart/form-data">
-                        <input type="password" class="form-control" style="width: 400px;margin-bottom: 20px"
-                               autofocus="" required="" id="password" name="password">
+    <p>Please enter the decryption password in the box below:</p>
 
-                        <button class='btn btn-lg btn-success' type="submit" name="download" id="download">Download
-                        </button>
-                    </form>
-                </center>
-            </div>
-            <?php include './res/footer.php'; ?>
-        </div>
+    <div class="center-form">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <input type="password" class="form-control" style="width: 400px;margin-bottom: 20px"
+                   autofocus="" required="" id="password" name="password">
+
+            <button class='btn btn-lg btn-success' type="submit" name="download" id="download">Download
+            </button>
+        </form>
     </div>
 </div>
+<?php include './res/footer.php'; ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="./js/bootstrap.min.js"></script>
 <script src="./js/docs.min.js"></script>
