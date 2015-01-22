33fff7b87d1aa12f33ea7d730d2dd74c7a787e69
psgs <psgs@users.noreply.github.com>
2015-01-23 09:37:49 +1000

    Fix "contributors" spelling in footer.php

--- res/footer.php
+++ res/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <p>
-        Made by some very dedicated <a href="https://github.com/Carlgo11/UploadMe/graphs/contributors">contributers</a>. The project is available on
+        Made by some very dedicated <a href="https://github.com/Carlgo11/UploadMe/graphs/contributors">contributors</a>. The project is available on
         <a href="https://github.com/Carlgo11/UploadMe">GitHub</a>
     </p>
 </footer>
