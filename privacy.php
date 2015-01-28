be85205b5d06436a52e9a357096c5b68bdc1a4ea
Carlgo11 <carlgo11@carlgo11.com>
2015-01-28 20:48:59 +0100

    Making privacy.php not centered. @psgs feel free to do this in a better way ;)

--- privacy.php
+++ privacy.php
@@ -1,16 +1,36 @@
-<?php include './res/head.php'; ?>
-<body>
-
-<?php
-include './res/navbar.php';
-getNavBar("privacy")
-?>
-<div class="content privacy">
-    <h1>Privacy</h1>
-    <?php
-    include './res/markdown.php';
-    echo markdown("./res/", "privacy.txt");
-    ?>
-</div>
-<?php include './res/footer.php'; ?>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Simple way to upload files">
+        <meta name="author" content="carlgo11">
+        <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+        <META HTTP-EQUIV="Expires" CONTENT="-1">
+        <link rel="shortcut icon" type="image/icon" href="./res/media/logo.png"/>
+        <title>
+            <?php
+            include './config.php';
+            echo $conf['title'];
+            ?>
+        </title>
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <link href="./css/stylesheet_no_centering.css" rel="stylesheet">
+        <script src="./js/bootstrap.file-input.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <?php
+        include './res/navbar.php';
+        getNavBar("privacy")
+        ?>
+        <div class="content privacy">
+            <h1>Privacy</h1>
+            <?php
+            include './res/markdown.php';
+            echo markdown("./res/", "privacy.txt");
+            ?>
+        </div>
+        <?php include './res/footer.php'; ?>
+    </body>
