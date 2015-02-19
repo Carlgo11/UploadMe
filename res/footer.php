0c55daf846354a8e35851b175dc7af076e7afb64
Carlgo11 <carlgo11@carlgo11.com>
2015-02-19 14:49:19 +0100

    Remove GleSYS logo per requst by GleSYS

--- res/footer.php
+++ res/footer.php
@@ -1,6 +1,4 @@
 <footer>
-    <span>Proudly supported by <a href="http://www.glesys.com/"><img src="./res/media/glesys.png" height="100" width="100"></a></span>
-
     <p>
         Made by some very dedicated <a href="https://github.com/Carlgo11/UploadMe/graphs/contributors">contributors</a>.
         The project is available on
