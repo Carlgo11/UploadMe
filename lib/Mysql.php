1fa20fd7fcc2e0f2cb2f7ac6dd170b48d11bca5b
Carlgo11 <carlgo11@carlgo11.com>
2015-03-12 21:34:09 +0100

    fix transparency report

--- lib/Mysql.php
+++ lib/Mysql.php
@@ -12,4 +12,21 @@ class Mysql {
         return($num > 0);
     }
 
+    public static function transparencyreport($name) {
+        global $database, $config;
+        $g = "SELECT `name`, `accepted`, `total` FROM `" . $config['tr-table'] . "` WHERE `name` = ?";
+        $query = $database->prepare($g);
+        $query->bind_param("s", $name);
+        $query->execute();
+        $query->bind_result($name, $accepted, $total);
+        if ($row = $query->fetch()) {
+            
+        }
+        if (!($accepted || $total) == 0) {
+            $p = $accepted / $total;
+        } else {
+            $p = 0;
+        }
+        return $accepted . "/" . $total . " (" . $p . "%)";
+    }
 }
